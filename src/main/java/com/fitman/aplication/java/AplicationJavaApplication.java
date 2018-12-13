7839ffaf6feb5e5a7a70c38176e9bc7fd38c4c0f
Pierre Antoine <Pierre.antoine03@live.fr>
2018-12-13 09:34:41 +0100

    premier commit

---
+++ src/main/java/com/fitman/aplication/java/AplicationJavaApplication.java
@@ -0,0 +1,14 @@
+package com.fitman.aplication.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationJavaApplication.class, args);
+	}
+
+}
+
